d5777929c027942713b2059e5fbdf60ea83f1fe4
aaa <1377869909@qq.com>
2019-11-12 21:32:04 +0800

    登录和个人信息

--- routes/api.php
+++ routes/api.php
@@ -261,7 +261,7 @@ $api->version('v1',[
 // 删除token
     $api->delete('qq/authorizations/current', 'LoginController@destroy')
         ->name('qq.authorizations.destroy');
-    $api->group(['middleware' => 'qq.auth'], function($api) {
+    $api->group(['middleware' => 'api.auth'], function($api) {
         // 当前登录用户信息
         $api->get('qq/user', 'LoginController@me')
             ->name('api.user.show');
