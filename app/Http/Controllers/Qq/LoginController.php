356052d1fcfbaa956d938065fc74b4fa8405f2a1
aaa <1377869909@qq.com>
2019-11-12 10:07:42 +0800

    qq登录接口调试

--- app/Http/Controllers/Qq/LoginController.php
+++ app/Http/Controllers/Qq/LoginController.php
@@ -2,12 +2,40 @@
 
 namespace App\Http\Controllers\Qq;
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    public function index()
+    public function login(Request $request)
     {
-        dd(1);
+        //基础信息获取
+        $appid = env('QQ_APP_ID','');
+        $screat = env('QQ_APP_SECRET','');
+        $js_code = $request->code;
+        $grant_type = 'authorization_code';
+        //获取session
+        $session = $this->getSession($appid,$screat,$js_code,$grant_type);
+
+    }
+
+
+
+
+
+
+
+    protected function getSession($appid,$screat,$js_code,$grant_type)
+    {
+        $session_url = 'https://api.q.qq.com/sns/jscode2session?appid='.$appid.'&secret='.$screat.'&js_code='.$js_code.'&grant_type='.$grant_type;
+        //获取session
+        $client = new Client();
+        $res = $client->request('GET', $session_url);
+        if ($res->getStatusCode() != 200) {
+            return $this->response->error('源服务器错误', 500);
+        }
+        $body = $res->getBody();
+        dd($body);
+
     }
 }
