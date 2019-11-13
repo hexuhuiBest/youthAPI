6d7c7bfe211395e9a0a5499cbfd25f05445d9220
aaa <1377869909@qq.com>
2019-11-13 12:30:40 +0800

    登录和个人信息

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -53,7 +53,6 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
-        'qq.auth' => \Illuminate\Auth\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
