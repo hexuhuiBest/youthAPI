bc8f0eb36e07640697e0a516404f6f0d41b2cd51
aaa <1377869909@qq.com>
2019-11-21 22:37:13 +0800

    图片上传建表

---
+++ database/migrations/2019_11_21_223544_create_images_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('event_id')->default(0);
+            $table->text('path');
+            $table->string('filename');
+            $table->text('mini_path');
+            $table->enum('type', ['police_event', 'police_certificate','insurance_standard','insurance_payment','hospital_injury','hospital_detail','passage']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
