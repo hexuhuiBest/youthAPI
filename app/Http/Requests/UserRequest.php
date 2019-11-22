06f1087ad3244e760191048d61ad3cc321d08e78
aaa <1377869909@qq.com>
2019-11-22 10:40:13 +0800

    图片上传

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -26,6 +26,7 @@ class UserRequest extends FormRequest
 
         return [
             'pictures' => 'mimes:jpeg,bmp,png,gif|dimensions:min_width=200,min_height=200',
+            'type'=>'required'
         ];
     }
     public function messages()
