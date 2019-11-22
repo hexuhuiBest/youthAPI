4f12f64386be50f21f31178d457404af64e6853e
aaa <1377869909@qq.com>
2019-11-22 10:53:48 +0800

    图片上传

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -33,7 +33,7 @@ class ImageUploadHandler
 //        $file_orig->move($upload_path.'/orig',$filename);
         // 将图片移动到我们的目标存储路径中
         $file->move($upload_path, $filename);
-
+        $copy = copy($upload_path.'/'.$filename,$upload_path.'/origin/'.$filename);
         // 如果限制了图片宽度，就进行裁剪
         if ($max_width && $extension != 'gif') {
 
@@ -43,7 +43,7 @@ class ImageUploadHandler
 
         return [
             'mini_path' => config('app.url') . "/$folder_name/$filename",
-//            'path' => config('app.url') . "/$folder_name".'/orig'."/$filename"
+            'path' => config('app.url') . "/$folder_name".'/orig'."/$filename"
         ];
     }
 
