397c30d417d0a91cf11a6932558f8104143577d3
aaa <1377869909@qq.com>
2019-11-24 19:02:49 +0800

    登录接口修改

--- app/Http/Controllers/Qq/LoginController.php
+++ app/Http/Controllers/Qq/LoginController.php
@@ -34,7 +34,7 @@ class LoginController extends Controller
         }
         //找到 openid 对应的用户
         $user = QqUser::where('qqapp_openid', $data['openid'])->first();
-        $usersBasic = QqUserBasic::where('user_id',$user->id);
+        $usersBasic = QqUserBasic::where('user_id',$user->id)->first();
         if (!$user) {
             $user = QqUser::create([
                 'qqapp_openid' => $data['openid'],
