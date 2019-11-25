02499f72459fe4d43b9d5a3c796355b0bdbb6d61
aaa <1377869909@qq.com>
2019-11-25 21:13:24 +0800

    列表点赞优化

--- app/Http/Controllers/Qq/Article.php
+++ app/Http/Controllers/Qq/Article.php
@@ -50,7 +50,6 @@ class Article extends Controller
 
     public function articleList()
     {
-        dd();
         $article = new QqArticle();
         $article = $article->orderBy('created_at','DESC')->paginate(10);
         return $this->response->paginator($article, new ArticleTransformer());
