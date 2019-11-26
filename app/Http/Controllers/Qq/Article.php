dd6923f4d3166309f5fa9475c537010b2ae43eef
aaa <1377869909@qq.com>
2019-11-26 17:32:52 +0800

    '收藏参数'

--- app/Http/Controllers/Qq/Article.php
+++ app/Http/Controllers/Qq/Article.php
@@ -58,6 +58,7 @@ class Article extends Controller
     public function zanArticle()
     {
         $zans = QqArticleGood::where('user_id', $this->user()->id)->pluck('article_id')->toArray();
+        dd($zans);
         $article = new QqArticle();
         $article = $article->whereIn('id', $zans)->orderBy('created_at', 'DESC')->get();
         dd($article);
