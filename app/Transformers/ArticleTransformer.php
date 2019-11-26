4a9da02e871e3c39d25d8499e0716dcd125f682a
aaa <1377869909@qq.com>
2019-11-26 11:51:38 +0800

    '列表信息'

--- app/Transformers/ArticleTransformer.php
+++ app/Transformers/ArticleTransformer.php
@@ -32,12 +32,12 @@ class ArticleTransformer extends TransformerAbstract
     public function users($imga)
     {
         $imga = QqUser::find($imga);
-         $imgas = QqUserBasic::where('user_id',$imga->id)->get();
+         $imgas = QqUserBasic::where('user_id',$imga->id)->first();
         return [
             'user_id' => $imga->id,
             'nickName' => $imga->nickName,
             'avatarUrl' => $imga->avatarUrl,
-             'gender' => $imgas->gender,
+            'gender' => $imgas->gender,
         ];
     }
     public function ImgTransformer($imga)
