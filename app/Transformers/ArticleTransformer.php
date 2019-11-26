d60dd954262e4af41c7b8601ec8f42f284e165e2
Fangjx <1935377905@qq.com>
2019-11-26 11:16:14 +0800

    transform返回gender

--- app/Transformers/ArticleTransformer.php
+++ app/Transformers/ArticleTransformer.php
@@ -8,6 +8,7 @@ use App\Models\QqArticle;
 use App\Models\QqArticleGood;
 use App\Models\QqComment;
 use App\Models\QqUser;
+use App\Models\QqUserBasic;
 use Auth;
 use Doctrine\DBAL\Schema\Schema;
 use League\Fractal\TransformerAbstract;
@@ -18,29 +19,31 @@ class ArticleTransformer extends TransformerAbstract
     {
         return [
             'id' => $article->id,
-            'content'=>$article->content,
-            'updated_at' =>$article->updated_at,
-            'user_info'=>$this->users($article->user_id),
+            'content' => $article->content,
+            'updated_at' => $article->updated_at,
+            'user_info' => $this->users($article->user_id),
             'pictures' => $this->ImgTransformer($article->pictures),
-            'count_comment'=>count(QqComment::where('article_id',$article->id)->get()),
-            'count_zan'=>count(QqArticleGood::where('article_id',$article->id)->get()),
-            'is_zan'=>is_null(QqArticleGood::where('article_id',$article->id)->where('user_id',Auth::guard('qq')->user()->id)->first())?0:1,
-            'type'=>$article->type,
-            ];
+            'count_comment' => count(QqComment::where('article_id', $article->id)->get()),
+            'count_zan' => count(QqArticleGood::where('article_id', $article->id)->get()),
+            'is_zan' => is_null(QqArticleGood::where('article_id', $article->id)->where('user_id', Auth::guard('qq')->user()->id)->first()) ? 0 : 1,
+            'type' => $article->type,
+        ];
     }
-    public function users($imga){
+    public function users($imga)
+    {
         $imga = QqUser::find($imga);
+        $imgas = QqUserBasic::find($imga);
         return [
-            'user_id'=>$imga->id,
-            'nickName'=>$imga->nickName,
-            'avatarUrl'=>$imga->avatarUrl,
-            'gender' =>$imga->gender,
+            'user_id' => $imga->id,
+            'nickName' => $imga->nickName,
+            'avatarUrl' => $imga->avatarUrl,
+            'gender' => $imgas->gender,
         ];
     }
     public function ImgTransformer($imga)
     {
         $array = [];
-        if(!is_null($imga)) {
+        if (!is_null($imga)) {
             foreach (json_decode($imga) as $key => $item) {
                 $img = Picture::find($item);
                 $imgs[$key] = $img->path;
