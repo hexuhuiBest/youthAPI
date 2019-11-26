f6a55217f859b4dba0e92bbbdd2ea89eee18cf83
aaa <1377869909@qq.com>
2019-11-26 16:50:57 +0800

    '点赞列表'

--- app/Http/Controllers/Qq/Article.php
+++ app/Http/Controllers/Qq/Article.php
@@ -57,7 +57,7 @@ class Article extends Controller
     }
     public function zanArticle()
     {
-        $zans = QqArticleGood::where('user_id',$this->user()->id)->plck('article_id')->toArray();
+        $zans = QqArticleGood::where('user_id',$this->user()->id)->pluck('article_id')->toArray();
         dd($zans);
         $article = new QqArticle();
         $article = $article->orderBy('created_at','DESC')->paginate(10);
