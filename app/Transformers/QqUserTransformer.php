ab8b8603173a46944a41e96388dab4a86a9fbfc5
aaa <1377869909@qq.com>
2019-11-26 09:23:12 +0800

    用户文章

--- app/Transformers/QqUserTransformer.php
+++ app/Transformers/QqUserTransformer.php
@@ -2,8 +2,9 @@
 
 namespace App\Transformers;
 
-use App\Http\Controllers\Qq\Fans;
+
 use App\Models\QqArticle;
+use App\Models\QqFans;
 use App\Models\QqUser;
 use App\Models\QqUserBasic;
 use League\Fractal\TransformerAbstract;
@@ -32,8 +33,8 @@ class QqUserTransformer extends TransformerAbstract
             'last_actived_at' => $user->last_actived_at,
             'created_at' => $user->created_at->toDateTimeString(),
             'updated_at' => $user->updated_at->toDateTimeString(),
-            'count_fans'=>count(Fans::where('fans_id',Auth::guard('qq')->user()->id)->get()),
-            'count_faned'=>count(Fans::where('fans_id',Auth::guard('qq')->user()->id)->get()),
+            'count_fans'=>count(QqFans::where('fans_id',Auth::guard('qq')->user()->id)->get()),
+            'count_faned'=>count(QqFans::where('user_id',Auth::guard('qq')->user()->id)->get()),
             'count_article'=>count(QqArticle::where('user_id',Auth::guard('qq')->user()->id)->get())
         ];
     }
