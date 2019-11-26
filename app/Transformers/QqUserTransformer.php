0cfa3a12365265a71aeeb6f7cb17a9b1eb930bdf
aaa <1377869909@qq.com>
2019-11-26 09:20:25 +0800

    用户基本信息

--- app/Transformers/QqUserTransformer.php
+++ app/Transformers/QqUserTransformer.php
@@ -2,6 +2,8 @@
 
 namespace App\Transformers;
 
+use App\Http\Controllers\Qq\Fans;
+use App\Models\QqArticle;
 use App\Models\QqUser;
 use App\Models\QqUserBasic;
 use League\Fractal\TransformerAbstract;
@@ -30,6 +32,9 @@ class QqUserTransformer extends TransformerAbstract
             'last_actived_at' => $user->last_actived_at,
             'created_at' => $user->created_at->toDateTimeString(),
             'updated_at' => $user->updated_at->toDateTimeString(),
+            'count_fans'=>count(Fans::where('fans_id',Auth::guard('qq')->user()->id)->get()),
+            'count_faned'=>count(Fans::where('fans_id',Auth::guard('qq')->user()->id)->get()),
+            'count_article'=>count(QqArticle::where('user_id',Auth::guard('qq')->user()->id)->get())
         ];
     }
 }
